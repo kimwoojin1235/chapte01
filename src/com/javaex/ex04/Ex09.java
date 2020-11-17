4d93a4e578ebfe86aaf729c9731d256f38f78005
kimwoojin1235 <ghkauszoqcu@naver.com>
2020-11-17 17:55:24 +0900

    수업내용

--- src/com/javaex/ex04/Ex09.java
+++ src/com/javaex/ex04/Ex09.java
@@ -6,13 +6,14 @@ public class Ex09 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while (true) {
+		boolean action = true;
+		while (action) {//true
 			System.out.println("숫자를 입력하세요");
 			System.out.print("숫자: ");
 			int num = sc.nextInt();
 			if (num==0) {
 				System.out.println("종료");		
-				break;
+				action = false;//true를 fales로 바꿔 브레이크 역할을함
 			}else if (num%3==0) {
 				System.out.println("3의 배수입니다.");
 			}else{
