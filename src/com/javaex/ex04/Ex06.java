1ac01da2958bb64e0d5d52ff47ee3659925e7bdf
kimwoojin1235 <ghkauszoqcu@naver.com>
2020-11-17 10:19:46 +0900

    tndjq

--- src/com/javaex/ex04/Ex06.java
+++ src/com/javaex/ex04/Ex06.java
@@ -5,11 +5,13 @@ public class Ex06 {
 	public static void main(String[] args) {
 		int i = 0;
 		
-		for (i=1; i<=5; i++) {
-				System.out.print("*****");
+		for (i=1; i<=6; i++) {
+			
+			for(int y=1; y<=i; y++) {
+				System.out.print("*");
+			}
 			System.out.println();
-		}
 		
+	     }
 	}
-
 }
--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -3,6 +3,7 @@ package com.javaex.ex04;
 public class Ex07 {
 
 	public static void main(String[] args) {
+		
 		for (int i = 0; i <5; i++) {
 			for (int st = 0; st <=i; st++) {
 				System.out.print("*");
